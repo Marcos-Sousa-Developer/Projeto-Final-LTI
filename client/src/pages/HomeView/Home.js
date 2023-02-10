3cdd672daaf6cd8dc53b209b83021a7a6b8a3edb
pedromoita <pedrocmoita2001@gmail.com>
2023-02-10 17:16:18 +0000

    começo no front end

--- client/src/pages/HomeView/Home.js
+++ client/src/pages/HomeView/Home.js
@@ -1,33 +1,15 @@
 import logo from '../../assets/images/logo.svg';
 import UseFetch from '../../hooks/UseFetch';
-import { Link  } from "react-router-dom" 
+import { Link  } from "react-router-dom";
+import { Navbar } from '../../components/';
 
 function Home() {   
 
   const {state} = UseFetch('/api')
 
   return (
-    
-    <div className="App"> 
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <li> <Link to="/admin">Admin</Link></li>
-        <p>
-          {state}  
-        </p> 
-        <p>
-          Bem vindo ao GreaterGoods Shop.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <Navbar></Navbar>
     </div>
   );
 }
