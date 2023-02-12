bbd3c7b6352facc1564458aa2159ad67425d8a63
pedromoita <pedrocmoita2001@gmail.com>
2023-02-12 19:16:28 +0000

    menu do navbar funciona

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,47 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { RxPerson, RxHamburgerMenu } from 'react-icons/rx';
-import { FiShoppingCart, FiSearch } from 'react-icons/fi'
+import { FiShoppingCart, FiSearch } from 'react-icons/fi';
+import { MdOutlineClose } from 'react-icons/md';
 
 import images from '../../assets/images.js';
 import './Navbar.css';
 
 const Navbar = () => {
-  return (
-    <div className='app__navbar'>
-        <div className='app__navbar__menu'>
-            <RxHamburgerMenu fontSize={30} color="black"></RxHamburgerMenu>
-            <img src={images.logo} alt="" />
-        </div>
+    const [toggleMenu, setToggleMenu] = useState(false);
 
-        <div className='app__navbar__searchBar'>
-            <div>
-                <FiSearch fontSize={22} color='black'></FiSearch>
-                <input type="text" placeholder='Pesquise...'/>
+    return (
+        <nav className='app__navbar app__container'>
+            <div className='app__navbar__menu'>
+                <RxHamburgerMenu fontSize={30} color="black" onClick={() => setToggleMenu(true)}></RxHamburgerMenu>
+                {toggleMenu && (
+                    <div className="app__navbar__menu_categories slide-right">
+                        <div>
+                            <MdOutlineClose fontSize={30} color="black" onClick={() => setToggleMenu(false)}></MdOutlineClose>
+                            <img src={images.logo} alt="" style={{marginLeft:'1rem'}}/>
+                        </div>
+                        <ul>
+                            <li>Categoria 1</li>
+                            <li>Categoria 2</li>
+                            <li>Categoria 3</li>
+                            <li>Categoria 4</li>
+                        </ul>
+                    </div>
+                )}
+                <img src={images.logo} alt="" />
             </div>
-        </div>
 
-        <div className='app__navbar__profile flex'>
-            <div className='flex'>
-                <RxPerson fontSize={30} color="black"></RxPerson>
+            <div className='app__navbar__searchBar'>
+                <div>
+                    <FiSearch fontSize={22} color='black'></FiSearch>
+                    <input type="text" placeholder='Pesquise...'/>
+                </div>
+            </div>
 
-                <div style={{margin: '0 .75rem'}}>
-                    <p>Account</p>
-                    <p style={{fontSize: '12px', opacity: '80%'}}>Sign In</p>
+            <div className='app__navbar__profile flex'>
+
+                <div className='flex'>
+                    <RxPerson fontSize={30} color="black"></RxPerson>
+
+                    <div style={{margin: '0 .75rem'}}>
+                        <p>Account</p>
+                        <p style={{fontSize: '12px', opacity: '80%'}}>Sign In</p>
+                    </div>
+
                 </div>
 
-            </div>
+                <div className='flex'>
+                    <FiShoppingCart fontSize={30} color="black"></FiShoppingCart>
 
-            <div className='flex'>
-                <FiShoppingCart fontSize={30} color="black"></FiShoppingCart>
+                    <div style={{margin: '0 .75rem'}}>
+                        <p>Total</p>
+                        <p style={{fontSize: '12px', opacity: '80%'}}>0.00€</p>
+                    </div>
 
-                <div style={{margin: '0 .75rem'}}>
-                    <p>Total</p>
-                    <p style={{fontSize: '12px', opacity: '80%'}}>0.00€</p>
                 </div>
-
             </div>
-        </div>
-    </div>
+        </nav>
   )
 }
 
