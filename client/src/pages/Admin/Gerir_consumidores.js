7aac263cf41218c5ee1dac1700a7da05ccc9d937
Marcos-Sousa-Developer <marcosleitao2000@gmail.com>
2023-02-15 23:54:49 +0000

    hooks add

--- client/src/pages/Admin/Gerir_consumidores.js
+++ client/src/pages/Admin/Gerir_consumidores.js
@@ -1,22 +1,15 @@
 import React from 'react'
-import {useState,useEffect} from 'react' 
-import getConsumers from './components/hooks/getConsumers'
+import getConsumers from '../../hooks/getConsumers'
 
 function Gerir_consumidores() { 
 
-  //const [users, setUsers] = useState([]);
-
   const users = getConsumers('/api/consumers') 
-
-  console.log(users)
-  
-  
 
   return ( 
     <div>
           {
             users.map((user) => (
-              <div className=''> -> {user.name},{user.id}</div>
+              <div className=''> {user.name},{user.id} </div>
             ))
           }
 
--- client/src/pages/HomeView/Home.js
+++ client/src/pages/HomeView/Home.js
@@ -1,11 +1,7 @@
-import UseFetch from '../../hooks/UseFetch';
-import { Link  } from "react-router-dom";
 import { Navbar } from '../../components/';
 import { Footer } from '../../components/';
 
 function Home() {
-
-  const {state} = UseFetch('/api')
 
   return (
     <>
