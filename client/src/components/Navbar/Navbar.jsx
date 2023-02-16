2e54224ab15dcd295fcb8dd7c5d713391cce7dc5
pedromoita <pedrocmoita2001@gmail.com>
2023-02-16 18:48:04 +0000

    navbar 100% responsive

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
                             <FiX fontSize={30} color="black" className='app__pointer app__icon_effect' onClick={() => setToggleMenu(false)}></FiX>
                             <img src={images.logo} alt="" className="app__logo"/>
                             <div></div>
-                            <ul className=''>
-                                <li>Categoria 1</li>
-                                <li>Categoria 2</li>
-                                <li>Categoria 3</li>
-                                <li>Categoria 4</li>
+                            <ul className='app_categories'>
+                                <li><a href="" className="app__text_effect">Categoria 1</a></li>
+                                <li><a href="" className="app__text_effect">Categoria 2</a></li>
+                                <li><a href="" className="app__text_effect">Categoria 3</a></li>
+                                <li><a href="" className="app__text_effect">Categoria 4</a></li>
                             </ul>
                         </div>
                     </div>
@@ -38,8 +38,8 @@ const Navbar = () => {
                     <FiUser fontSize={30} color="black" className='app__pointer app__icon_effect'></FiUser>
 
                     <div className="app__navbar_profile_account" style={{margin: '0 .75rem'}}>
-                        <p>Account</p>
-                        <p style={{fontSize: '12px', opacity: '80%'}}>Sign In</p>
+                        <p>Conta</p>
+                        <p style={{fontSize: '12px', opacity: '80%'}}>Sign Up</p>
                     </div>
 
                 </div>
