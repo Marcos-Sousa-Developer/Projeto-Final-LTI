70d3748748a28917686fd76b75c84abf8bced82f
mleita02Dev <marcos.leitao@decathlon.com>
2023-03-02 11:09:53 +0000

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ function App() {
 
   return (
     <BrowserRouter forceRefresh={true}>
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
