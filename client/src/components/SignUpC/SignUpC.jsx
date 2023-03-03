5e697e7f5c62f2a5ae52634dfe4250e729c5a22c
Gabriel <gabrielrfonseca02@gmail.com>
2023-03-03 00:00:12 +0000

    Sign Up

--- client/src/components/SignUpC/SignUpC.jsx
+++ client/src/components/SignUpC/SignUpC.jsx
@@ -1,16 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FiMail, FiLock } from 'react-icons/fi';
 import './SignUp.css';
 
-const SignUpC = () => {
-  return (
-    <>
-      <div>SignUpC</div>
-      <div>SignUpC</div>
-      <div>SignUpC</div>
-      <div>SignUpC</div>
-      <div>SignUpC</div>
-    </>
-  )
-}
+const SignInC = () => {
+    return (
+        <>
+        <h1>Sign Up</h1>
+        <div className='app__SignUp'>
+          <div className='app__SignUp_box'>
+              <div className='app__SignUp_box1'>
+                  <form>
+                      <div>
+                          <p>E-mail</p>
+                          <div className='app__SignUp_box11'>
+                              <FiMail fontSize={22} color='black'  aria-hidden="true"/>
+                              <input
+                                  name=""
+                                  id=""
+                                  type="text"
+                                  required
+                              />
+                          </div>
+                      </div>
+                      <div>
+                          <p>Password</p>
+                          <div className='app__SignUp_box11'>
+                              <FiLock fontSize={22} color='black'  aria-hidden="true"/>
+                              <input
+                                  name=""
+                                  id=""
+                                  type="password"
+                                  required
+                              />
+                          </div>
+                      </div>
+                      <div className='app__SignUp_box13'>
+                      <button type='submit'>
+                          Sign Up
+                      </button>
+                      <p>Não tem conta? Sign Up.</p>
+                      </div>
+                  </form>
+              </div>
 
-export default SignUpC
+              <div className='app__SignUp_box2'>
+                  <p>or</p>    
+
+              </div>
+              <div className='app__SignUp_box3'>
+                    <button className='app__SignUp_box30 ButtonGoogle'>
+                        Google
+                    </button>
+                    <button className='app__SignUp_box30 ButtonFacebook'>
+                        Facebook
+                    </button>
+                    <button className='app__SignUp_box30 ButtonTwitter'>
+                        Twitter
+                    </button>
+              </div>
+          </div>    
+    </div>  
+    </>
+    )
+  }
+  
+  export default SignInC
+
+
