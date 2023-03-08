aa9b73c27dceb7d3b161364d322c3c4d2dbe4cb4
pedromoita <pedrocmoita2001@gmail.com>
2023-03-08 18:34:28 +0000

    correção de erros e melhorias no SignIn

--- client/src/components/SignInC/SignInC.jsx
+++ client/src/components/SignInC/SignInC.jsx
@@ -1,69 +1,71 @@
 import React, { useState } from 'react';
-import { FiMail, FiLock } from 'react-icons/fi';
+import { FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 import './SignIn.css';
+import images from '../../assets/images';
 
 const SignInC = () => {
     return (
-        <>
-        <h1>Sign In</h1>
-        <div className='app__SignIn'>
-          <div className='app__SignIn_box'>
-              <div className='app__SignIn_box1'>
-                  <form>
-                      <div>
-                          <p>E-mail</p>
-                          <div className='app__SignIn_box11'>
-                              <FiMail fontSize={22} color='black'  aria-hidden="true"/>
-                              <input
-                                  name=""
-                                  id=""
-                                  type="text"
-                                  required
-                              />
-                          </div>
-                      </div>
-                      <div>
-                          <p>Password</p>
-                          <div className='app__SignIn_box11'>
-                              <FiLock fontSize={22} color='black'  aria-hidden="true"/>
-                              <input
-                                  name=""
-                                  id=""
-                                  type="password"
-                                  required
-                              />
-                          </div>
-                      </div>
-                      <div className='app__SignIn_box13'>
-                      <button type='submit'>
-                          Login
-                      </button>
-                      <p>Não tem conta? Sign Up.</p>
-                      </div>
-                  </form>
-              </div>
+    <>
+        <img className='app__SignIn_rectangle1' src={images.Rectangle1} alt="" />
+        <img className='app__SignIn_rectangle2' src={images.Rectangle2} alt="" />
+        <div className='app__SignIn main__container'>
+            <Link to='/'><FiArrowLeft></FiArrowLeft></Link>
+            <h1 className=''>Sign In</h1>
+                <div className='app__SignIn_box'>
+                    <div className='app__SignIn_box1'>
+                        <form>
+                            <div>
+                                <p>E-mail</p>
+                                <div className='app__SignIn_box11'>
+                                    <FiMail fontSize={22} color='black'  aria-hidden="true"/>
+                                    <input
+                                        name=""
+                                        id=""
+                                        type="text"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <p>Password</p>
+                                <div className='app__SignIn_box11'>
+                                    <FiLock fontSize={22} color='black'  aria-hidden="true"/>
+                                    <input
+                                        name=""
+                                        id=""
+                                        type="password"
+                                        required
+                                    />
+                                </div>
+                                <p>Esqueceu-se da password?</p>
+                            </div>
+                            <div className='app__SignIn_box13'>
+                            <button className='main__action_btn' type='submit'>Login</button>
+                            <p>Não tem conta? Sign Up.</p>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='app__SignIn_box2'>
+                        <p>or</p>    
 
-              <div className='app__SignIn_box2'>
-                  <p>or</p>    
-
-              </div>
-              <div className='app__SignIn_box3'>
-                    <button className='app__SignIn_box30 ButtonGoogle'>
-                        Google
-                    </button>
-                    <button className='app__SignIn_box30 ButtonFacebook'>
-                        Facebook
-                    </button>
-                    <button className='app__SignIn_box30 ButtonTwitter'>
-                        Twitter
-                    </button>
-              </div>
-          </div>    
-    </div>  
+                    </div>
+                    <div className='app__SignIn_box3'>
+                            <button className='app__SignIn_box30 ButtonGoogle'>
+                                Google
+                            </button>
+                            <button className='app__SignIn_box30 ButtonFacebook'>
+                                Facebook
+                            </button>
+                            <button className='app__SignIn_box30 ButtonTwitter'>
+                                Twitter
+                            </button>
+                    </div>
+                </div>    
+        </div>
     </>
     )
-  }
+}
   
-  export default SignInC
-
-
+export default SignInC
--- client/src/pages/HomeView/SignIn.js
+++ client/src/pages/HomeView/SignIn.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Footer } from '../../components/';
-import { SignInC } from '../../components/';
+import { Footer } from '../../components';
+import { SignInC } from '../../components';
 
 const SignIn = () => {
   return (
