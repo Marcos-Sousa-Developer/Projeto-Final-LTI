576c802417b28e1c9f2ad598cdc54d559286ea23
Gabriel <gabrielrfonseca02@gmail.com>
2023-03-14 18:14:31 +0000

    Sign up

--- client/src/components/SignUpC/SignUpC.jsx
+++ client/src/components/SignUpC/SignUpC.jsx
@@ -1,69 +1,80 @@
 import React, { useState } from 'react';
-import { FiMail, FiLock } from 'react-icons/fi';
+import { FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 import './SignUp.css';
+import images from '../../assets/images';
 
-const SignInC = () => {
+const SignUpC = () => {
     return (
-        <>
-        <h1>Sign Up</h1>
-        <div className='app__SignUp'>
-          <div className='app__SignUp_box'>
-              <div className='app__SignUp_box1'>
-                  <form>
-                      <div>
-                          <p>E-mail</p>
-                          <div className='app__SignUp_box11'>
-                              <FiMail fontSize={22} color='black'  aria-hidden="true"/>
-                              <input
-                                  name=""
-                                  id=""
-                                  type="text"
-                                  required
-                              />
-                          </div>
-                      </div>
-                      <div>
-                          <p>Password</p>
-                          <div className='app__SignUp_box11'>
-                              <FiLock fontSize={22} color='black'  aria-hidden="true"/>
-                              <input
-                                  name=""
-                                  id=""
-                                  type="password"
-                                  required
-                              />
-                          </div>
-                      </div>
-                      <div className='app__SignUp_box13'>
-                      <button type='submit'>
-                          Sign Up
-                      </button>
-                      <p>Não tem conta? Sign Up.</p>
-                      </div>
-                  </form>
-              </div>
+    <>
+        <img className='app__SignUp_rectangle1' src={images.Rectangle1} alt="" />
+        <img className='app__SignUp_rectangle2' src={images.Rectangle2} alt="" />
+        <div className='app__SignUp main__container'>
+            <Link to='/'><FiArrowLeft></FiArrowLeft></Link>
+            <h1 className=''>Sign Up</h1>
+                <div className='app__SignUp_box'>
+                    <div className='app__SignUp_box1'>
+                        <form>
+                            <div>
+                                <p>E-mail</p>
+                                <div className='app__SignUp_box11'>
+                                    <FiMail fontSize={22} color='black'  aria-hidden="true"/>
+                                    <input
+                                        name=""
+                                        id=""
+                                        type="text"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <p>Password</p>
+                                <div className='app__SignUp_box11'>
+                                    <FiLock fontSize={22} color='black'  aria-hidden="true"/>
+                                    <input
+                                        name=""
+                                        id=""
+                                        type="password"
+                                        required
+                                    />
+                                </div>
+                            </div>
 
-              <div className='app__SignUp_box2'>
-                  <p>or</p>    
+                            <div className='app__SignUp_box131'>
+                            <p>É um vendedor ou comprador</p>
+                              <div className='app__SignUp_box131_Buttons'>
+                              <button className='main__action_btn buttonComprador' type='submit'>Comprador</button>
+                              <button className='main__action_btn buttonVendedor' type='submit'>Vendedor</button>
+                              </div>
+                            </div>
 
-              </div>
-              <div className='app__SignUp_box3'>
-                    <button className='app__SignUp_box30 ButtonGoogle'>
-                        Google
-                    </button>
-                    <button className='app__SignUp_box30 ButtonFacebook'>
-                        Facebook
-                    </button>
-                    <button className='app__SignUp_box30 ButtonTwitter'>
-                        Twitter
-                    </button>
-              </div>
-          </div>    
-    </div>  
+                            <div className='app__SignUp_box13'>
+                            <button className='main__action_btn' type='submit'>Sign Up</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='app__SignUp_box2'>
+                        <p>or</p>    
+
+                    </div>
+                    <div className='app__SignUp_box3'>
+                            <button className='app__SignUp_box30 ButtonGoogle'>
+                                Google
+                            </button>
+                            <button className='app__SignUp_box30 ButtonFacebook'>
+                                Facebook
+                            </button>
+                            <button className='app__SignUp_box30 ButtonTwitter'>
+                                Twitter
+                            </button>
+                    </div>
+                </div>    
+        </div>
     </>
     )
-  }
+}
   
-  export default SignInC
+export default SignUpC
 
 
