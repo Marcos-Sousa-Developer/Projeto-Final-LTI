328b147d8420d5ff5ba4ddc55fcf04fd58e4488a
pedromoita <pedrocmoita2001@gmail.com>
2023-03-14 18:56:34 +0000

    .

--- client/src/components/Navbar/NavbarSupplier.jsx
+++ client/src/components/Navbar/NavbarSupplier.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiUser} from 'react-icons/fi';
 
 import images from '../../assets/images.js';
+import './styles/Navbar.css';
 
 const NavbarSupplier = () => {
   return (
     <>
-        <Link to='/'><img src={images.logo} alt="" className=""/></Link>
+        <div className='app__navbarSupplier main__container'>
+            <Link to='/'><img src={images.logo} alt="" className=""/></Link>
 
-        <div className='app__navbar_profile'>
+            <div className='app__navbar_profile'>
 
-            <Link to="/signin" className="flex app__pointer app__navbar_links">
-                <FiUser fontSize={30} color="black" className='profile_icon'></FiUser>
+                <Link to="/signin" className="flex app__pointer app__navbar_links">
+                    <FiUser fontSize={30} color="black" className='profile_icon'></FiUser>
 
-                <div className="app__navbar_profile_account" style={{margin: '0 .75rem'}}>
-                    <span className="profile_link">Conta</span>
-                    <p style={{fontSize: '12px', opacity: '80%'}}>Iniciar sessão</p>
-                </div>
-            </Link>
+                    <div className="app__navbar_profile_account" style={{margin: '0 .75rem'}}>
+                        <span className="profile_link">Conta</span>
+                        <p style={{fontSize: '12px', opacity: '80%'}}>Iniciar sessão</p>
+                    </div>
+                </Link>
 
-            <Link to="/" className="flex app__pointer app__navbar_links"  style={{marginRight:'0'}}>
-                Anunciar
-            </Link>
+                <Link to="/" className="flex app__pointer app__navbar_links"  style={{marginRight:'0'}}>
+                    Anunciar
+                </Link>
 
+            </div>
         </div>
     </>
   )
---
+++ client/src/pages/HomeView/CriarAnuncio/CriarAnuncio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {NavbarSupplier, Footer} from '../../../components/index';
+
+
+function CriarAnuncio() {
+
+  return (
+    <>
+        <NavbarSupplier></NavbarSupplier>
+        <div>CriarAnuncio</div>
+        <Footer></Footer>
+    </>
+  )
+
+}
+
+export default CriarAnuncio
