7c79d494d17e43e6f49ba05b50e4dc36f8b7c8b4
pedromoita <pedrocmoita2001@gmail.com>
2023-03-24 20:50:00 +0000

    cont CriarAnuncio

--- client/src/pages/HomeView/CriarAnuncio/CriarAnuncio.js
+++ client/src/pages/HomeView/CriarAnuncio/CriarAnuncio.js
@@ -34,7 +34,7 @@ function CriarAnuncio() {
             return URL.createObjectURL(file);
         });
 
-        if (selectedImages.length + imagesArray.length <= 10) {
+        if (selectedImages.length + imagesArray.length <= 8) {
             setSelectedImages((previousImages) => previousImages.concat(imagesArray));
         }
 
@@ -51,64 +51,66 @@ function CriarAnuncio() {
             <form onSubmit={handleSubmit} className='app__anuncio_content' id='anuncio_form'>
                 <div className='app__anuncio_content-product'>
                     <p>Dados do Produto</p>
-                    <div className='app__anuncio_inputArea app__anuncio_content-product_area'>
-                        <div className='app__anuncio_content-product_area-g1'>
-                            <InputField title='Título' inputype='text'></InputField>
-                            <InputField title='Preço' inputype='number'></InputField>
-                            <div>
-                                <p>Descrição</p>
-                                <textarea 
-                                    style={{width:'100%', maxHeight: '150px', minHeight:'75px', resize:'vertical', outline:'none', border: '3px solid #EEEEEE', borderRadius:'10px', padding:'0.25rem 0.5rem'}} 
-                                    form='anuncio_form' 
-                                    maxLength="300" 
-                                    onInput={(e) => {
-                                        if (e.target.value.length >= 300) {
-                                            alert(' Atingiu o limite máximo de caracteres');
-                                        }
-                                        setText(e.target.value);
-                                    }}>
-                                </textarea>
-                                <span style={{fontSize: '.75rem'}}>{text.length > 0 ? text.length : ''}</span>
-                            </div>
-                        </div>
-                        <div className='app__anuncio_content-product_area-g2'>
-                            <InputField title='Data de produção' inputype='date'></InputField>
-                            <div>
-                                <p>Categoria</p>
-                                <button className='main__action_btn'>Escolher</button>
-                            </div>
-                            <div>
-                                <p>Imagens <span style={{fontSize: '.75rem'}}>(máx. 10)</span></p>
-                                {selectedImages.length > 0 &&
-                                    <button onClick={() => setSelectedImages([])}>limpar tudo</button>
-                                }
-                                <div className='teste'>
-                                    {selectedImages.length === 10 &&
-                                        <p>Atingiu o limite de imagens!</p>
-                                    }
-                                    {selectedImages.length < 10 &&
-                                        <label className='app__anuncio_image_input'>
-                                            <div>
-                                                <FiPlus style={{textAlign: 'center'}}></FiPlus>
-                                            </div>
-                                            <input type="file" accept="image/*" multiple onChange={onSelectFile}/>
-                                        </label>
-                                    }
-                                    <div className='app__anuncio_images_selected'>
-                                        {selectedImages &&
-                                            selectedImages.map((image, index)=>{
-                                                return(
-                                                    <div key={image} className='app__anuncio_image_selected'>
-                                                        <img src={image} alt='' className='app__anuncio_image_selected_img'/>
-                                                        <FiX className='trash' onClick={() => setSelectedImages(selectedImages.filter((e) => e !== image))}></FiX>
-                                                        {/*<p>{index + 1}</p>*/}
-                                                    </div>
-                                                )
-                                            })
-                                        }
-                                    </div>
+                    <div className='app__anuncio_inputArea'>
+                        <div className='app__anuncio_content-product_area'>
+                            <div className='app__anuncio_content-product_area-g1'>
+                                <InputField title='Título' inputype='text'></InputField>
+                                <InputField title='Preço' inputype='number'></InputField>
+                                <div>
+                                    <p>Descrição</p>
+                                    <textarea 
+                                        style={{width:'100%', maxHeight: '150px', minHeight:'75px', resize:'vertical', outline:'none', border: '3px solid #EEEEEE', borderRadius:'10px', padding:'0.25rem 0.5rem'}} 
+                                        form='anuncio_form' 
+                                        maxLength="300" 
+                                        onInput={(e) => {
+                                            if (e.target.value.length >= 300) {
+                                                alert(' Atingiu o limite máximo de caracteres');
+                                            }
+                                            setText(e.target.value);
+                                        }}>
+                                    </textarea>
+                                    <span style={{fontSize: '.75rem'}}>{text.length > 0 ? text.length : ''}</span>
                                 </div>
                             </div>
+                            <div className='app__anuncio_content-product_area-g2'>
+                                <InputField title='Data de produção' inputype='date'></InputField>
+                                <div>
+                                    <p>Categoria</p>
+                                    <button className='main__action_btn'>Escolher</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='app__anuncio_image_section'>
+                            <p>Imagens <span style={{fontSize: '.75rem'}}>(máx. 8)</span></p>
+                            {selectedImages.length > 0 &&
+                                <button onClick={() => setSelectedImages([])}>limpar tudo</button>
+                            }
+                            <div className='app__anuncio_image_section-content'>
+                                {selectedImages.length < 8 &&
+                                    <label className='app__anuncio_image_input'>
+                                        <div>
+                                            <FiPlus style={{textAlign: 'center'}}></FiPlus>
+                                        </div>
+                                        <input type="file" accept="image/*" multiple onChange={onSelectFile}/>
+                                    </label>
+                                }
+                                <div className='app__anuncio_images_selected'>
+                                    {selectedImages &&
+                                        selectedImages.map((image, index)=>{
+                                            return(
+                                                <div key={image} className='app__anuncio_image_selected'>
+                                                    <img src={image} alt='' className='app__anuncio_image_selected_img'/>
+                                                    <FiX className='trash' onClick={() => setSelectedImages(selectedImages.filter((e) => e !== image))}></FiX>
+                                                    {/*<p>{index + 1}</p>*/}
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            {selectedImages.length === 8 &&
+                                <p style={{margin: '0', color: '#EB5C1F'}}>Atingiu o limite de imagens!</p>
+                            }
                         </div>
                     </div>
                 </div>
@@ -124,6 +126,7 @@ function CriarAnuncio() {
                             <InputField title='Localização' inputype='text'></InputField>
                         </div>
                     </div>
+                    <a className='.app__text_effect'>Pré-visualizar</a>
                     <button type='submit' className='main__action_btn flex'>Publicar</button>
                 </div>
             </form>
