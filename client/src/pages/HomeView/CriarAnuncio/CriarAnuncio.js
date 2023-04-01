4b098f81fd3581dfc10c9c636e81026cd5a9eb30
pedromoita <pedrocmoita2001@gmail.com>
2023-04-01 22:57:55 +0100

    .

--- client/src/pages/HomeView/CriarAnuncio/CriarAnuncio.js
+++ client/src/pages/HomeView/CriarAnuncio/CriarAnuncio.js
@@ -4,7 +4,6 @@ import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
 import {NavbarSupplier, Footer, SubHeading, InputField} from '../../../components/index';
 import GeneralInfo from './GeneralInfo';
 import ProductInfo from './ProductInfo';
-import SupplierInfo from './SupplierInfo';
 import { categories } from '../../../utilities/categories';
 import "./CriarAnuncio.css";
 import "../../../components/InputField/InputField.css";
@@ -29,15 +28,13 @@ function CriarAnuncio() {
         localizacao: "",
     });
 
-    const FormTitles = ["Dados Gerais", "Detalhes do Produto", "Dados de Contacto"];
+    const FormTitles = ["Dados Gerais", "Detalhes do Produto"];
 
     const PageDisplay = () => {
       if (page === 0) {
         return <GeneralInfo formData={formData} setFormData={setFormData} />;
-      } else if (page === 1) {
-        return <ProductInfo formData={formData} setFormData={setFormData} />;
       } else {
-        return <SupplierInfo formData={formData} setFormData={setFormData} />;
+        return <ProductInfo formData={formData} setFormData={setFormData} />;
       }
     };
 
@@ -46,40 +43,28 @@ function CriarAnuncio() {
         <NavbarSupplier></NavbarSupplier>
         <div className='app__anuncio main__container'>
             <SubHeading title="Criar anúncio"></SubHeading>
-            <div className='app__anuncio_content' id='anuncio_form' style={{marginTop:'1rem'}}>
+            <form className='app__anuncio_content' id='anuncio_form' style={{marginTop:'1rem'}}>
                 <p>{FormTitles[page]}</p>
                 <div className='app__anuncio_inputArea'>
                     <div className='app__anuncio_progressBar'>
-                        <span style={{color: page == 0 ? 'black' : 'white', backgroundColor: page == 0 ? 'white' : 'rgba(235, 92, 31, 0.1)'}}>
-                            {page == 0 ? '30%' : page == 1 ? '70%' : '100%'}
+                        <span>
+                            {page == 0 ? '50%' : '100%'}
                         </span>
-                        <div style={{ width: page == 0 ? "33.3%" : page == 1 ? "66.6%" : "100%" }}></div>
+                        <div style={{ width: page == 0 ? "50%"  : "100%" }}></div>
                     </div>
                     {PageDisplay()}
                 </div>
                 <div className='app__anuncio_content_stepBtns' style={{ justifyContent: page == 0 ? "flex-end" : "space-between"}}>
-                    <button
-                        disabled={page == 0}
-                        onClick={() => {
-                            setPage((currPage) => currPage - 1);
-                            useEffectd();
-                        }}
-                        className='main__action_btn' style={{display: page == 0 ? 'none' : 'inline'}}
-                    ><FiChevronLeft></FiChevronLeft> Anterior</button>
-                    <button
-                        onClick={() => {
-                            if (page === FormTitles.length - 1) {
-                                alert("FORM SUBMITTED");
-                                console.log(formData);
-                            } else {
-                                setPage((currPage) => currPage + 1);
-                                useEffectd();
-                            }
-                        }}
-                        className='main__action_btn'
-                    >{page === FormTitles.length - 1 ? "Publicar " : "Continuar "}<FiChevronRight></FiChevronRight></button>
+                    {page == 0 ?
+                            <button type='button' onClick={() => {setPage((currPage) => currPage + 1); useEffectd()}} className='main__action_btn'>Continuar <FiChevronRight></FiChevronRight></button>
+                    :
+                        <>
+                            <button type='button' onClick={() => { setPage((currPage) => currPage - 1); useEffectd(); }} className='main__action_btn'><FiChevronLeft></FiChevronLeft> Anterior</button>
+                            <button type='button' onClick={() => {console.log(formData)}} className='main__action_btn'>Publicar <FiChevronRight></FiChevronRight></button>
+                        </>
+                    } 
                 </div>
-            </div>
+            </form>
         </div>
         <Footer></Footer>
     </>
