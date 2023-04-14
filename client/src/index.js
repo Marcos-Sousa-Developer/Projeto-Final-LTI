3ccd770de3e32af249d532a8bfa3f1a5dbb48356
mleita02Dev <marcos.leitao@decathlon.com>
2023-04-14 19:34:23 +0100

    delete logs

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <React.StrictMode>
     <BrowserRouter forceRefresh={true}>
       <App />
     </BrowserRouter>
+  </React.StrictMode>
 );
 
 
