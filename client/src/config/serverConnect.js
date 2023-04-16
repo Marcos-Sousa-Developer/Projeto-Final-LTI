418451a6121c4903f24c562e9980c9db4de1d661
mleita02Dev <marcos.leitao@decathlon.com>
2023-04-16 02:01:58 +0100

    save

--- client/src/config/serverConnect.js
+++ client/src/config/serverConnect.js
@@ -1,9 +1,4 @@
-let API_URL = ""
 
-if (process.env.REACT_APP_ENV === "local") {
-
-    API_URL = process.env.REACT_APP_SERVER_HOST
-
-}
+let API_URL = process.env.REACT_APP_SERVER_HOST
 
 export default API_URL 
