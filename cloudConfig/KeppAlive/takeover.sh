85229b0f91902f47ffd516d573e6fd4bfdf0c1d9
mleita02Dev <marcos.leitao@decathlon.com>
2023-04-20 12:42:55 +0100

    add

--- cloudConfig/KeppAlive/takeover.sh
+++ cloudConfig/KeppAlive/takeover.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
-if  /usr/bin/pgrep nginx > /dev/null
+ip=$(curl -s ifconfig.me)
+
+if  [ "$ip" == "34.175.17.77" ]; 
 then
     exit 0
 else
+    
+    if timeout 10s telnet 10.255.128.64 80 > /dev/null; then
 
-    ip=$(curl -s ifconfig.me)
-
-    if [ "$ip" == "34.175.17.77" ]; then
-
+        exit 0
+    else
         # Unassign peer's IP aliases. Try it until it's possible.
         gcloud compute instances delete-access-config load-balancer-1 --zone=europe-southwest1-a --access-config-name='external-nat' --quiet >> /etc/keepalived/takeover.log 2>&1 &
         sleep 5
@@ -17,10 +19,7 @@ else
         # Assign IP aliases to me because now I am the MASTER!
         gcloud compute instances add-access-config load-balancer-2 --zone=europe-southwest1-a --access-config-name=external-nat --address=34.175.17.77 --quiet >> /etc/keepalived/takeover.log 2>&1 &
         sleep 5
-
-        exit 1
-    else
-        echo "Port already changed" >> /etc/keepalived/takeover.log
+        echo "Im a new master: load-balancer-1" >> /etc/keepalived/takeover.log
         
         exit 1
     fi
