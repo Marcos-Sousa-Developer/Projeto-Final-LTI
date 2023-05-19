ea414ccccf315890a48ffac9afdc99979bb65a66
Miguel Fernandes <miguelferreirafernandes@hotmail.com>
2023-05-19 21:20:29 +0100

    UPDATED

--- client/src/pages/HomeView/SupplierOrdersHistory/SupplierOrdersHistory.js
+++ client/src/pages/HomeView/SupplierOrdersHistory/SupplierOrdersHistory.js
@@ -7,70 +7,82 @@ import './SupplierOrdersHistory.css';
 
 const SupplierOrdersHistory = () => {
     const [ordersHistory, setSupplierOrder] = useState([]);
-    const [modalOpen, setModalOpen] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     async function getSupplierOrder(){
       let supplierOrdersGet = await getAllFromDB("/orderedProducts", {product_owner_uid: true})
       if(typeof supplierOrdersGet != "string") {
-          setSupplierOrder(prevState => [...supplierOrdersGet])
+          //setSupplierOrder(prevState => [...supplierOrdersGet])
+          return supplierOrdersGet
       }
+    }
+
+    async function getAd(data){
+      let lista = []
+      data.map(async(orderHistory) => {
+        let adGet = await getAllFromDB("/ads/" + orderHistory.ad_id)
+        lista.push([orderHistory, adGet[0].title])
+        console.log(12312)
+      })
+      setSupplierOrder(lista)
     }
 
     //Aparecer no loading da página
     useEffect(()=>{
-        getSupplierOrder()
+      setLoading(true)
+      async function run(){
+        let data = await getSupplierOrder()
+        await getAd(data)
+        setLoading(false)
+      }
+      run()
     }, [])
+
+    function test(oi) {
+      console.log(Object.keys(oi))
+    }
 
     return (
     <>
-    <NavbarSupplier></NavbarSupplier>
-    <div className='app__prod-unit main__container'>
-      <SubHeading title="Histórico de encomendas"/>
-        {ordersHistory.length > 0 && (
-          <>
-            <table className='app__prod-unit_existing-units'>
-              <thead>
-                <tr>
-                  <th>Número da encomenda</th>
-                  <th></th>
-                  <th>Localização do comprador</th>
-                  <th>Total €</th>
-                  <th>Estado</th>
-                </tr>
-              </thead>
-              <tbody>
-                {ordersHistory.map((orderHistory, index) => (
-                  <React.Fragment key={index}>
-                    <tr>
-                      <td>{orderHistory.order_id}</td>
-                      <td>
-                        <button onClick={() => setModalOpen(prevState => {
-                          const newState = [...prevState];
-                          newState[index] = true;
-                          return newState;
-                        })}><FiPlusCircle></FiPlusCircle></button>
-                        <Modal open={modalOpen[index]} onClose={() => setModalOpen(prevState => {
-                          const newState = [...prevState];
-                          newState[index] = false;
-                          return newState;
-                        })}>
-                          <p>Categoria: {orderHistory.product_category}</p>
-                          <p>Subcategoria: {orderHistory.product_subcategory}</p>
-                          <p>Subsubcategoria: {orderHistory.product_subsubcategory}</p>
-                        </Modal>
-                      </td>
-                      <td>{orderHistory.buyer_location}</td>
-                      <td>{orderHistory.price}</td>                      
-                      <td>{orderHistory.order_status}</td>
-                    </tr>
-                  </React.Fragment>
-                ))}
-              </tbody>
-            </table>
-          </>
-        )}
-      </div>
-    <Footer></Footer>
+    {
+      loading ? "loading":
+      <>
+      <NavbarSupplier></NavbarSupplier>
+      <div className='app__prod-unit main__container'>
+        <SubHeading title="Histórico de encomendas"/>
+        <button onClick={() => {console.log(ordersHistory)}}>QWEWQEQWE</button>
+          {ordersHistory.length > 0 && (
+            <>
+              <table className='app__prod-unit_existing-units'>
+                <thead>
+                  <tr>
+                    <th>Número da encomenda</th>
+                    <th>Nome do produto</th>
+                    <th>Localização do comprador</th>
+                    <th>Total €</th>
+                    <th>Estado</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {ordersHistory.map((orderHistory) => (
+                    <React.Fragment key={orderHistory.id}>
+                      <tr>
+                        <td>{orderHistory.order_id}</td>
+                        <td>{test(orderHistory)??"ok"}</td>
+                        <td>{orderHistory.buyer_location}</td>
+                        <td>{orderHistory.price}</td>                      
+                        <td>{orderHistory.order_status}</td>
+                      </tr>
+                    </React.Fragment>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          )}
+        </div>
+      <Footer></Footer>
+      </>
+    }
     </>
   )
 }
