c1bb4a7e1ffbcbc2c6b3b315b29d72b660f5a38f
Gabriel <gabrielrfonseca02@gmail.com>
2023-06-20 01:20:02 +0100

    transp

--- client/src/pages/HomeView/TransportadoraIndex/TransportadoraIndex.js
+++ client/src/pages/HomeView/TransportadoraIndex/TransportadoraIndex.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import { authentication as auth}  from '../../authentication'
-//import './TransportadoraIndex.css';
+import './TransportadoraIndex.css';
 import {Footer} from '../../../components/index';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link } from "react-router-dom";
@@ -8,55 +8,64 @@ import { Link } from "react-router-dom";
 function TransportadoraIndex() { 
 
   return (
-    <div>
-      <Link to='/signin'><FiArrowLeft></FiArrowLeft></Link>
-      <h3 className=''>Transportadora</h3>
-
-
-
-      <p>Crendencial de Acesso</p>
-          <div className='app__SignIn_box11'>
-              <div></div>
-              <input
-                  required
-                  type="text" 
-                  className="form-control"
-              />
-          </div>
-      <p>Código de Acesso</p>
-        <div className='app__SignIn_box11'>
-            <div></div>
-            <input
-                required
-                type="text" 
-                className="form-control"
-            />
-        </div>
-      <p>Número de Encomenda</p>
-        <div className='app__SignIn_box11'>
-            <div></div>
-            <input
-                required
-                type="text" 
-                className="form-control"
-            />
-        </div>
-      <p>Código de Validação</p>
-        <div className='app__SignIn_box11'>
-            <div></div>
-            <input
-                required
-                type="text" 
-                className="form-control"
-            />
-        </div>
-      <button className='main__action_btn'>Login</button>
-
+    <>
+    
+    <div className='app__Transportadora main__container'>
+    <Link to='/signin'><FiArrowLeft></FiArrowLeft></Link>
+    <h3 className=''>Transportadora</h3>
       
 
+
+      <div className='app__Transportadora_box'>
+            <div className='app__Transportadora_box1'>
+            <p>Crendencial de Acesso</p>
+                <div className='app__Transportadora_box11'>
+                    
+                    <input
+                        required
+                        type="text" 
+                    />
+                </div>
+            </div>
+            <div className='app__Transportadora_box1'>
+            <p>Código de Acesso</p>
+                <div className='app__Transportadora_box11'>
+                    
+                    <input
+                        required
+                        type="text" 
+                    />
+                </div>
+            </div>
+            <div className='app__Transportadora_box1'>
+            <p>Número de Encomenda</p>
+                <div className='app__Transportadora_box11'>
+                    
+                    <input
+                        required
+                        type="text" 
+                    />
+                </div>
+            </div>
+            <div className='app__Transportadora_box1'>
+            <p>Código de Validação</p>
+                <div className='app__Transportadora_box11'>
+                    
+                    <input
+                        required
+                        type="text" 
+                    />
+                </div>
+            </div>
+        <div className='flex btn'>
+        <button className='main__action_btn'>Login</button>
+        </div>
+      </div>
       
+
+      </div>
         <Footer></Footer>
-    </div>
+    </>
   );
 }
 
