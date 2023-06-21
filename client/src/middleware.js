ee8809d12b7948b3d2c983856f629e9685b59481
Marcos-Sousa-Developer <marcosleitao2000@gmail.com>
2023-06-21 01:50:45 +0100

    save

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -6,17 +6,22 @@ if(process.env.REACT_APP_ENV == 'local') {
   axiosConfig = () => {
     // Set the common headers
     axios.defaults.headers.common['identification'] = process.env.REACT_APP_ID_KEY;
-    axios.defaults.withCredentials = true
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.withCredentials = true,
     axios.defaults.baseURL = "http://localhost:5000/api";
-
+    axios.credentials = 'include'
   };
 }
 else {
   axiosConfig = () => {
     // Set the common headers
     axios.defaults.headers.common['identification'] = process.env.REACT_APP_ID_KEY;
-    axios.defaults.withCredentials = true
+    axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.withCredentials = true,
     axios.defaults.baseURL = "https://greatergoods.pt/api";
+    axios.credentials = 'include'
   };
 }
 
